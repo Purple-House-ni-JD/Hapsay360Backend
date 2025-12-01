58e25239127dc706b91f73dd7e5a5f3de0a82346
Akane230 <dagoc.kharljey@gmail.com>
2025-12-02 01:13:20 +0800

    Added the auth middleware in the police station routes

--- src/routes/policeStation.routes.js
+++ src/routes/policeStation.routes.js
@@ -5,10 +5,15 @@ import {
   deletePoliceStation,
 } from "../controllers/policeStation.controller.js";
 
+import {
+  authMiddleware,
+  authorizeRoles,
+} from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.post("/create", createPoliceStation);
-router.get("/getStations", getStations);
-router.delete("/delete/:id", deletePoliceStation);
+router.post("/create", authMiddleware, authorizeRoles("admin"), createPoliceStation);
+router.get("/getStations", authMiddleware, authorizeRoles("admin"), getStations);
+router.delete("/delete/:id", authMiddleware, authorizeRoles("admin"), deletePoliceStation);
 
 export default router;
