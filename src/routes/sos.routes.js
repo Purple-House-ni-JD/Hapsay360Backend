5f50afb9456bb09d7b8513d7ec1aef57535e6d59
John Dave Rojo <rojo.dave2004@gmail.com>
2025-12-05 02:36:16 +0800

    sos admin page can search, filter, edit, assigned station

--- src/routes/sos.routes.js
+++ src/routes/sos.routes.js
@@ -1,8 +1,27 @@
 import express from "express";
-import { createSOS } from "../controllers/sos.controller.js";
+import {
+  createSOS,
+  getAllSOS,
+  getSOSById,
+  updateSOS,
+  deleteSOS,
+} from "../controllers/sos.controller.js";
 
 const router = express.Router();
 
+// Create new SOS request
 router.post("/create", createSOS);
+
+// Get all SOS requests
+router.get("/", getAllSOS);
+
+// Get single SOS request by ID
+router.get("/:id", getSOSById);
+
+// Update SOS request
+router.put("/:id", updateSOS);
+
+// Delete SOS request
+router.delete("/:id", deleteSOS);
 
 export default router;
