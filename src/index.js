d2591fa372929685f3d48eed8d0284d887b7c9e7
Akane230 <dagoc.kharljey@gmail.com>
2025-12-06 09:17:13 +0800

    deleted uploads

--- src/index.js
+++ src/index.js
@@ -28,9 +28,6 @@ app.use(
 app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ extended: true, limit: '16mb' }));
 
-// Serve static files from uploads directory
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-
 // Mount all routes with /api prefix
 app.use("/api", routes);
 
